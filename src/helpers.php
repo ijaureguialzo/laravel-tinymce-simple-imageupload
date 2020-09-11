9fde1a70171422a453bb650196a45b11c4950e9c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-09-11 08:33:13 +0200

    Don't add img to the file url

--- src/helpers.php
+++ src/helpers.php
@@ -5,14 +5,14 @@ if(! function_exists('mce_back'))
     /**
      * Respond a script back to TinyMCE
      *
-     * @param  string $filename
+     * @param  string $url
      * @return string
      */
-    function mce_back($filename)
+    function mce_back($url)
     {
         return ("
             <script>
-                top.$('.tox-browse-url').parent().find('.tox-textfield').val('/img/$filename');
+                top.$('.tox-browse-url').parent().find('.tox-textfield').val('$url');
                 top.$(\"button[title|='Save']\").click();
             </script>
         ");
